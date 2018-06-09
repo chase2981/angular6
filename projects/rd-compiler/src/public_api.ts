3f1852497e21e4ab26e9adbc70c43eabdcab0627
Chase Gibbons <chase@rentdynamics.com>
2018-06-08 22:48:17 -0600

    renamed libs to shorter variations of the same things

--- projects/rd-compiler/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of rd-compiler
- */
-
-export * from './lib/rd-compiler.service';
-export * from './lib/rd-compiler.component';
-export * from './lib/rd-compiler.module';
--- projects/rd-core/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of rd-core
- */
-
-export * from './lib/rd-core.service';
-export * from './lib/rd-core.component';
-export * from './lib/rd-core.module';
